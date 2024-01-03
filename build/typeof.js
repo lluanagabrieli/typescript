0677f5cd61ff0606bc56da6366582b940b01c966
lluanagabrieli <luanagabrieli4599@hotmail.com>
2024-01-02 22:07:06 -0300

    :sparkles: estudos sobre typeof em ts

--- build/typeof.js
+++ build/typeof.js
@@ -1,5 +1,6 @@
 "use strict";
 // TYPEOF - operador do JS que retorna uma string com o tipo do dado.
+// é um type guard, utilizado para dizer que a minha variável é de determinado tipo, inserindo métodos que condizem com o tipo
 // pode retornar: string, number, boolean, function, object, undefined, bigint e symbol.
 // tipos primitivos(básicos) do ts
 const frase = "Front End";
@@ -14,7 +15,7 @@ if (typeof preco === "number") {
 if (typeof condi === "boolean") {
     console.log("condi é um boolean");
 }
-//String, Number e Boolena é diferente de string, number e boolean. São funcões construtoras desses tipos de dados, responsáveis pela herança das propriedades e métodos dos mesmos.
+//String, Number e Boolean é diferente de string, number e boolean. São funcões construtoras desses tipos de dados, responsáveis pela herança das propriedades e métodos dos mesmos.
 const frase1 = new String("Front End");
 const frase2 = String("Front End");
 const frase3 = "Front End";
